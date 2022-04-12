e60f7e2bbe79ba94229a1ce4f6ae94fae480e2ee
Amisha Agarwal <Amisha.Agarwal@mechlintech.com>
2022-04-12 19:17:52 +0530

    Assgn

--- 2.py
+++ 2.py
@@ -1,1 +1,12 @@
 # Combination Sum
+from itertools import combinations
+def combinationsum(candidate,target):
+    ls1=[]
+    for i in range(len(candidate)+1):
+        for j in combinations(candidate,i):
+            if sum(j)==target:
+                ls1.append(j)
+    print(ls1) 
+    
+    
+combinationsum([1,2,3,5],5)  
