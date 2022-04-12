a798078c776175c0de9e1a64f91739a3bc6ae8f0
Amisha Agarwal <Amisha.Agarwal@mechlintech.com>
2022-04-12 10:23:39 +0530

    Assgn

--- 3.py
+++ 3.py
@@ -1,14 +1,18 @@
 # Roman number to Integers 
-s=input()
-roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-ls=[]
-sum=0
-for i in roman:
-    for j in s:
-        if j==i:
-            A=roman[j]
-            ls.append(A)
+def romantoint(s):
+    
+    roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    ls=[]
+    sum=0
+    for i in roman:
+        for j in s:
+            if j==i:
+                A=roman[j]
+                ls.append(A)
             
-for k in range(0,len(ls)):
-    sum=sum+ls[k]
-print(sum)
+    for k in range(0,len(ls)):
+        sum=sum+ls[k]
+    return sum
+        
+romantoint('VI')       
+        
