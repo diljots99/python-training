3316129d2b9859f9365b227391c7fa0c919fa2cc
Amisha Agarwal <Amisha.Agarwal@mechlintech.com>
2022-04-12 10:10:28 +0530

    Assgn

--- 1.py
+++ 1.py
@@ -1,2 +1,12 @@
 # Two Sums
 # Hi Amisha 
+def twosum(nums,target):
+
+    for i in range (0,len(nums)):
+        for j in range (1,len(nums)):
+            if nums[i]+nums[j]==target:
+                return i,j
+            
+print(twosum([7,5,1],6))            
+            
+             
--- 3.py
+++ 3.py
@@ -1,1 +1,14 @@
 # Roman number to Integers 
+s=input()
+roman={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+ls=[]
+sum=0
+for i in roman:
+    for j in s:
+        if j==i:
+            A=roman[j]
+            ls.append(A)
+            
+for k in range(0,len(ls)):
+    sum=sum+ls[k]
+print(sum)
