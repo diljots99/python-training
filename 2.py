63b83c52e9b4902bfa815e188ffcfe023e73490c
vishesh vishwakarma <vishesh.vishwakarma@mechlintech.com>
2022-04-13 18:22:21 +0530

    updated

--- 2.py
+++ 2.py
@@ -15,3 +15,52 @@ array=[10,20,30,40,50,60,70,80,90]
 x=80
 combinationsum(array,x)
 
+
+
+def combinationSum(arr, sum):
+	ans = []
+	temp = []
+
+	
+	arr = sorted(list(set(arr)))
+	findNumbers(ans, arr, temp, sum, 0)
+	return ans
+
+def findNumbers(ans, arr, temp, sum, index):
+	
+	if(sum == 0):
+		
+		
+		ans.append(list(temp))  
+		return
+	
+	
+	for i in range(index, len(arr)):
+
+		
+		if(sum - arr[i]) >= 0:          # check for negativity
+
+			# adding element which can equal to sum
+			# sum
+			temp.append(arr[i])
+			findNumbers(ans, arr, temp, sum-arr[i], i)
+
+			
+			temp.remove(arr[i])
+
+
+
+arr = [2, 3, 5]
+sum = 8
+ans = combinationSum(arr, sum)
+
+
+if len(ans) <= 0:
+	print("no combination find")
+	
+
+print(ans)
+
+	
+
+
