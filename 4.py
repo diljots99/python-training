4fbce12e702dd650f85d929b39f8e36c574a744f
vishesh vishwakarma <vishesh.vishwakarma@mechlintech.com>
2022-04-13 18:22:44 +0530

    updated

--- 4.py
+++ 4.py
@@ -1,20 +1,25 @@
 # Count Servers that Communicate
-class solution:
-    
-    def countserver(self,grid:list[list[int]]) -> int:
-        m=len(grid)
-        n=len(grid[0])
-        ans=0
-        rows=[0]*m
-        cols=[0]*n
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]==1:
-                    rows[i]=rows[i]+1
-                    cols[j]=cols[j]+1
 
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]==1 and (rows[i]>1 or cols[j]>1):
-                    ans=ans+1
-            return ans
+    
+def countserver(grid:list[list[int]]) -> int:
+
+    m=len(grid)
+    n=len(grid[0])
+    ans=0
+    rows=[0]*m
+    cols=[0]*n
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j]==1:
+                rows[i]=rows[i]+1
+                cols[j]=cols[j]+1
+
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j]==1 and (rows[i]>=1 or cols[j]>=1):
+                ans=ans+1
+        return ans
+
+
+grid=[[1,0],[1,1]]
+countserver(grid)
